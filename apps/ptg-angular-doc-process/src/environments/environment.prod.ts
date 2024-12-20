3cef9ac346efb1a7fd68e621231a3b20cf1e6cb9
Lokesh Daiya <LDaiya@slb.com>
2024-12-20 11:56:58 +0530

    update baseurl

--- apps/ptg-angular-doc-process/src/environments/environment.prod.ts
+++ apps/ptg-angular-doc-process/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://ptg-ui-apps.azurewebsites.net/api/',
-  fileAccessPath: 'https://ptg-ui-apps.azurewebsites.net/uploads/',
-  signatureFilePath: 'https://ptg-ui-apps.azurewebsites.net/signatures/',
+  baseUrl: 'https://yash-ui-apps.azurewebsites.net/api/',
+  fileAccessPath: 'https://yash-ui-apps.azurewebsites.net/uploads/',
+  signatureFilePath: 'https://yash-ui-apps.azurewebsites.net/signatures/',
   filePath : "/assets/"
 };
