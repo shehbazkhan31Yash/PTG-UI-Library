b2d9a1140e7ca71c8a7c8893148cae318f123225
Manish  Patidar <patidar.manish@Yash.com>
2025-01-02 12:45:08 +0530

    changes

--- libs/ptg-ui-react-lib/src/lib/carousel/carousel.tsx
+++ libs/ptg-ui-react-lib/src/lib/carousel/carousel.tsx
@@ -22,70 +22,72 @@ export function PtgUiCarousel({
   showIndicators = true,
 }: CarouselProps) {
   return (
-    <div className="parent-container">
-      <div className="carousel">
-        <ul
-          className="slides"
-          style={{
-            minHeight: `${imgHeight}`,
-            width: `${imgWidth}`,
-          }}
-        >
-          {images.length
-            ? images.map((imgPath, index) => {
-                return (
-                  <div>
-                    <input
-                      type="radio"
-                      name="radio-buttons"
-                      id={`img-${index}`}
-                      checked
-                    />
-                    <li className="slide-container">
-                      <div className="slide-image">
-                        <img src={imgPath} alt="Image" />
-                      </div>
-                      <div
-                        className="carousel-controls"
-                        style={{ lineHeight: imgHeight }}
-                      >
-                        <label
-                          htmlFor={`img-${
-                            index == 0 ? images.length - 1 : index - 1
-                          }`}
-                          className="prev-slide"
+    <>
+      <div className="parent-container">
+        <div className="carousel">
+          <ul
+            className="slides"
+            style={{
+              minHeight: `${imgHeight}`,
+              width: `${imgWidth}`,
+            }}
+          >
+            {images.length
+              ? images.map((imgPath, index) => {
+                  return (
+                    <div>
+                      <input
+                        type="radio"
+                        name="radio-buttons"
+                        id={`img-${index}`}
+                        checked
+                      />
+                      <li className="slide-container">
+                        <div className="slide-image">
+                          <img src={imgPath} alt="Image" />
+                        </div>
+                        <div
+                          className="carousel-controls"
+                          style={{ lineHeight: imgHeight }}
                         >
-                          <span>&lsaquo;</span>
-                        </label>
-                        <label
-                          htmlFor={`img-${
-                            index == images.length - 1 ? 0 : index + 1
-                          }`}
-                          className="next-slide"
-                        >
-                          <span>&rsaquo;</span>
-                        </label>
-                      </div>
-                    </li>
-                  </div>
-                );
-              })
-            : null}
-          {showIndicators && (
-            <div className="carousel-dots">
-              {images.map((imgPath, index) => (
-                <label
-                  htmlFor={`img-${index}`}
-                  className="carousel-dot"
-                  id={`img-dot-${index}`}
-                  key={imgPath}
-                ></label>
-              ))}
-            </div>
-          )}
-        </ul>
+                          <label
+                            htmlFor={`img-${
+                              index == 0 ? images.length - 1 : index - 1
+                            }`}
+                            className="prev-slide"
+                          >
+                            <span>&lsaquo;</span>
+                          </label>
+                          <label
+                            htmlFor={`img-${
+                              index == images.length - 1 ? 0 : index + 1
+                            }`}
+                            className="next-slide"
+                          >
+                            <span>&rsaquo;</span>
+                          </label>
+                        </div>
+                      </li>
+                    </div>
+                  );
+                })
+              : null}
+            {showIndicators && (
+              <div className="carousel-dots">
+                {images.map((imgPath, index) => (
+                  <label
+                    htmlFor={`img-${index}`}
+                    className="carousel-dot"
+                    id={`img-dot-${index}`}
+                    key={imgPath}
+                  ></label>
+                ))}
+              </div>
+            )}
+          </ul>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
