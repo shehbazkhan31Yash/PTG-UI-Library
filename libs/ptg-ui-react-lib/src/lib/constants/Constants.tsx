8350f987beb843edcf2070dbf84367cb8f03cee9
ankita.keshri <ankita.keshri@yash.com>
2025-01-22 16:32:45 +0530

    constant file added

--- libs/ptg-ui-react-lib/src/lib/constants/Constants.tsx
+++ libs/ptg-ui-react-lib/src/lib/constants/Constants.tsx
@@ -1,10 +1,10 @@
-export interface CardButton {
+export interface ICardButton {
     TEXT_COLOR: string;
     COLOR: string;
     WIDTH: string;
 }
 
-export const CARD_BUTTON: CardButton = {
+export const CARD_BUTTON: ICardButton = {
      TEXT_COLOR: '#fff',
      COLOR: '#052982',
      WIDTH: '165px',
