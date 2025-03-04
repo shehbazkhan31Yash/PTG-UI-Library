0c05e865b2bf9bc7624910a7450dcabef00c4078
Krishna Vaibhav <krishna.vaibhav@yash.com>
2025-03-04 15:36:12 +0530

    Changes regarding msal config url

--- apps/ptg-react-app/src/environments/environment.prod.ts
+++ apps/ptg-react-app/src/environments/environment.prod.ts
@@ -5,8 +5,8 @@ export const environment = {
   baseUrl: baseUrl,
   login: `${baseUrl}auth/local`,
   signup: `${baseUrl}auth/local/register`,
-  msal_postLogoutRedirectUri: 'https://ptg-ui-apps.azurewebsites.net/',
-  msal_redirectUri: 'https://ptg-ui-apps.azurewebsites.net/',
+  msal_postLogoutRedirectUri: 'https://yash-ui-apps.azurewebsites.net/',
+  msal_redirectUri: 'https://yash-ui-apps.azurewebsites.net/',
   msal_clientId: '688a4bcc-30fe-44ce-88ca-9e9750714b92',
   msal_authority:
     'https://login.microsoftonline.com/yashtechnologies841.onmicrosoft.com',
