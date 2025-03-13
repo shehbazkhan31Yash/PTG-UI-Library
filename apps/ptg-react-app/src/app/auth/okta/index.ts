599d81579dbca12caeab3287ec458f47885238e1
Krishna Vaibhav <krishna.vaibhav@yash.com>
2025-03-13 13:18:02 +0530

    Moved the configuration related to okta in same config file

--- apps/ptg-react-app/src/app/auth/okta/index.ts
+++
@@ -1,9 +0,0 @@
-const oktaConfig = {
-  issuer: 'https://dev-mgbwsj7nuk623uv6.us.auth0.com/oauth2/default',
-  clientId: 'G2AaSk8Xs3SCnoKT1chhM1suw4ROqwZ5',
-  redirectUri: window.location.origin + '/login',
-  scopes: ['openid', 'profile', 'email'],
-  domain: 'dev-mgbwsj7nuk623uv6.us.auth0.com',
-};
-
-export default oktaConfig;
--- apps/ptg-react-app/src/environments/environment.ts
+++ apps/ptg-react-app/src/environments/environment.ts
@@ -14,6 +14,11 @@ export const environment = {
   msal_clientId: '688a4bcc-30fe-44ce-88ca-9e9750714b92',
   msal_authority:
     'https://login.microsoftonline.com/yashtechnologies841.onmicrosoft.com',
+  okta_issuer: 'https://dev-mgbwsj7nuk623uv6.us.auth0.com/oauth2/default',
+  okta_clientId: 'G2AaSk8Xs3SCnoKT1chhM1suw4ROqwZ5',
+  okta_redirectUri: 'http://localhost:4200/login',
+  okta_scopes: ['openid', 'profile', 'email'],
+  okta_domain: 'dev-mgbwsj7nuk623uv6.us.auth0.com',
 };
 
 /*
