092bf6f73f167b693b9a7b8e28d6cca8fd30caf8
Vijaya Jiwalag Tiple <vijaya.tiple@yash.com>
2025-03-18 17:22:33 +0530

    Added test cases for calendar component

--- libs/ptg-ui-react-lib/src/lib/calendar/calendar.spec.tsx
+++ libs/ptg-ui-react-lib/src/lib/calendar/calendar.spec.tsx
@@ -1,10 +1,47 @@
-import { render } from '@testing-library/react';
-
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 import { PtgUiCalendar } from './calendar';
 
-describe('Calendar', () => {
-	it('should render successfully', () => {
-		const { baseElement } = render(<PtgUiCalendar />);
-		expect(baseElement).toBeTruthy();
+describe('PtgUiCalendar', () => {
+	const defaultProps = {
+		className: 'test-class',
+		selected: new Date('2025-03-18'),
+		onChange: jest.fn(),
+		startDate: new Date('2025-01-01'),
+		endDate: new Date('2025-12-31'),
+		disabled: false,
+		isDateTime: false,
+	};
+
+	test('renders date input type', () => {
+		render(<PtgUiCalendar {...defaultProps} />);
+		const inputElement = screen.getByLabelText('Select date');
+		expect(inputElement).toHaveAttribute('type');
+	});
+
+	test('renders datetime-local input type', () => {
+		render(<PtgUiCalendar {...defaultProps} isDateTime={true} />);
+		const inputElement = screen.getByLabelText('Select date');
+		expect(inputElement).toHaveAttribute('type');
+	});
+
+	test('handles date changes', () => {
+		render(<PtgUiCalendar {...defaultProps} />);
+		const inputElement = screen.getByLabelText('Select date');
+		fireEvent.change(inputElement, { target: { value: '2025-03-19' } });
+		expect(defaultProps.onChange).toHaveBeenCalled();
+	});
+
+	test('checks if the input is disabled', () => {
+		render(<PtgUiCalendar {...defaultProps} disabled={true} />);
+		const inputElement = screen.getByLabelText('Select date');
+		expect(inputElement).toBeDisabled();
+	});
+
+	test('verifies the minimum and maximum selectable dates', () => {
+		render(<PtgUiCalendar {...defaultProps} />);
+		const inputElement = screen.getByLabelText('Select date');
+		expect(inputElement).toHaveAttribute('min');
+		expect(inputElement).toHaveAttribute('max');
 	});
 });
