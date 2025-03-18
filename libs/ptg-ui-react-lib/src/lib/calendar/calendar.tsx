bdeb942fe4bb0661da606f20b6e9013c59e4d82c
Vijaya Jiwalag Tiple <vijaya.tiple@yash.com>
2025-03-18 17:13:18 +0530

    Resolved PR comments

--- libs/ptg-ui-react-lib/src/lib/calendar/calendar.tsx
+++ libs/ptg-ui-react-lib/src/lib/calendar/calendar.tsx
@@ -18,17 +18,39 @@ import { IPtgUiCalendarProps } from '../interfaces';
  * @returns {JSX.Element} A JSX element representing the calendar input.
  */
 export const PtgUiCalendar = (props: Readonly<IPtgUiCalendarProps>) => {
+	const {
+		className,
+		selected,
+		onChange,
+		startDate,
+		endDate,
+		disabled,
+		isDateTime,
+	} = props;
+	const formatDate = (date: Date | string) => {
+		if (date instanceof Date) {
+			return date.toISOString().split('T')[0];
+		}
+		return date || '';
+	};
+	const formatDateTime = (date: Date | string) => {
+		if (date instanceof Date) {
+			return date.toISOString().slice(0, 16); // Format for datetime-local
+		}
+		return date || '';
+	};
 	return (
 		<div className="position-relative post-icon">
 			<div>
 				<input
-					className={props.className}
-					type={!props.isDateTime ? 'date' : 'datetime-local'}
-					value={props.selected instanceof Date ? props.selected.toISOString().split('T')[0] : props.selected || ''}
-					onChange={props.onChange}
-					min={props.startDate instanceof Date ? props.startDate.toISOString().split('T')[0] : props.startDate || ''}
-					max={props.endDate instanceof Date ? props.endDate.toISOString().split('T')[0] : props.endDate || ''}
-					disabled={props.disabled}
+					className={className}
+					type={isDateTime ? 'datetime-local' : 'date'}
+					value={isDateTime ? formatDateTime(selected ?? '') : formatDate(selected ?? '')}
+					onChange={onChange}
+					min={isDateTime ? formatDateTime(startDate ?? '') : formatDate(startDate ?? '')}
+					max={isDateTime ? formatDateTime(endDate ?? '') : formatDate(endDate ?? '')}
+					disabled={disabled}
+					aria-label="Select date"
 				/>
 			</div>
 		</div>
