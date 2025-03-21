425309251a4f042cb3451f299dbef945574fedef
Pratiksha Gaikwad <pratiksha.gaikwad@yash.com>
2025-03-21 11:27:15 +0530

    refactored input and checks component

--- libs/ptg-ui-react-lib/src/lib/checks/checks.tsx
+++ libs/ptg-ui-react-lib/src/lib/checks/checks.tsx
@@ -1,40 +1,28 @@
-/* eslint-disable jsx-a11y/no-access-key */
-/**
- * @since March 2022
- * @author Ankit Patidar
- * @desc Reusable Checkbox Component
- *
- */
-
+import { PtgUiCheckboxProps } from '@ptg-react-libs/interfaces';
 import './checks.scss';
-interface PtgUiCheckboxProps {
-	id?: string;
-	name?: string;
-	label?: string;
-	value?: string;
-	for?: string;
-	htmlFor?: string;
-	checked?: boolean;
-	onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
-	className?: string;
-	accessKey?: string;
-}
+/**
+ * PtgUiChecks Component
+ *
+ * A functional component that renders a customizable checkbox.
+ * 
+ * @param {Readonly<PtgUiCheckboxProps>} props - The props for the checkbox component.
+ * @param {string} props.id - The unique identifier for the checkbox.
+ * @param {string} props.name - The name attribute for the checkbox.
+ * @param {string} props.label - The label text for the checkbox.
+ * @param {string} props.value - The value attribute for the checkbox.
+ * @param {string} props.for - The function to call when the checkbox is clicked.
+ * @param {string} props.htmlFor - The id of the element that the label is bound to.
+ * @param {boolean} props.checked - Indicates if the checkbox is checked.
+ * @param {function} props.onChange - The function to call when the checkbox value changes.
+ * @param {string} props.className - The className for the checkbox.
+ *
+ * @returns {JSX.Element} A JSX element representing the checkbox.
+ */
 const defaultProps: PtgUiCheckboxProps = {
 	checked: false,
 };
-
-export function PtgUiCheckbox({
-	id,
-	name,
-	label,
-	value,
-	htmlFor,
-	checked,
-	onChange,
-	className,
-	accessKey,
-	...rest
-}: PtgUiCheckboxProps) {
+export function PtgUiCheckbox(props: Readonly<PtgUiCheckboxProps>) {
+	const { id, name, label, value, htmlFor, checked, onChange, className } = props;
 	return (
 		<div className="form-check">
 			<input
@@ -45,7 +33,6 @@ export function PtgUiCheckbox({
 				checked={checked}
 				className={className}
 				onChange={onChange}
-				accessKey={accessKey}
 				data-testid={name}
 			/>
 			<label className="form-check-label" htmlFor={htmlFor}>
