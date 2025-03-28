311656844bcd559f6b8619555544fd81bee3b57b
priyanka jain <priyanka.jain@yash.com>
2025-03-28 20:36:59 +0530

    storybook changes

--- libs/ptg-ui-angular-lib/.storybook/main.js
+++ libs/ptg-ui-angular-lib/.storybook/main.js
@@ -50,32 +50,3 @@ module.exports = {
     return config;
   },
 };
-// const rootMain = require('../../../.storybook/main');
-
-
-// module.exports = {
-//   ...rootMain,
-
-//   core: { ...rootMain.core, builder: 'webpack5' },
-
-//   stories: [
-//     ...rootMain.stories,
-//     '../**/*.stories.mdx',
-//     '../**/*.stories.@(js|jsx|ts|tsx)',
-//   ],
-//   addons: [...rootMain.addons,'@storybook/addon-docs','@storybook/addon-controls'],
-//   framework: {
-//     name: '@storybook/angular',
-//     options: {},
-//   },
-//   webpackFinal: async (config, { configType }) => {
-//     // apply any global webpack configs that might have been specified in .storybook/main.js
-//     if (rootMain.webpackFinal) {
-//       config = await rootMain.webpackFinal(config, { configType });
-//     }
-
-//     // add your own webpack tweaks if needed
-
-//     return config;
-//   },
-// };
