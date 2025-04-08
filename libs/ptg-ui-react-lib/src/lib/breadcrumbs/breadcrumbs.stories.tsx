41f0a0e51fb4133eace77621fc7364b8b2eb29cb
Vijaya Jiwalag Tiple <vijaya.tiple@yash.com>
2025-04-08 14:54:16 +0530

    Updated breadcrumbs story

--- libs/ptg-ui-react-lib/src/lib/breadcrumbs/breadcrumbs.stories.tsx
+++ libs/ptg-ui-react-lib/src/lib/breadcrumbs/breadcrumbs.stories.tsx
@@ -1,32 +1,55 @@
-import { Meta, StoryFn } from '@storybook/react';
+// stories.tsx
+
+import { StoryFn, Meta } from '@storybook/react';
 import { PtgUiBreadcrumbs } from './Breadcrumbs';
 import { IPtgUiBreadcrumbsProps } from '@ptg-react-libs/interfaces';
-import { BrowserRouter as Router } from 'react-router-dom';
-
 
 export default {
-    title: 'Components/Breadcrumbs',
+    title: 'Components/PtgUiBreadcrumbs',
     component: PtgUiBreadcrumbs,
 } as Meta;
 
-const Template: StoryFn<IPtgUiBreadcrumbsProps> = (args) => <Router><PtgUiBreadcrumbs {...args} /></Router>;
+const Template: StoryFn<IPtgUiBreadcrumbsProps> = (args) => <PtgUiBreadcrumbs {...args} />;
 
 export const Default = Template.bind({});
 Default.args = {
     datalist: [
         { title: 'Home', link: '/' },
-        { title: 'Library', link: '/library' },
-        { title: 'Data', link: '/data' },
+        { title: 'Nature', link: '/nature' },
+        { title: 'Forests', link: '/nature/forests' },
     ],
-    handleClick: () => alert('Breadcrumb clicked!'),
+    handleClick: () => console.log('Breadcrumb clicked!'),
 };
 
-export const WithoutLinks = Template.bind({});
-WithoutLinks.args = {
+export const ActiveBreadcrumb = Template.bind({});
+ActiveBreadcrumb.args = {
     datalist: [
-        { title: 'Home' },
-        { title: 'Library' },
-        { title: 'Data' },
+        { title: 'Home', link: '/' },
+        { title: 'Nature', link: '/nature' },
+        { title: 'Rivers', link: '/nature/rivers' },
+        { title: 'Amazon River' }, // No link for the active breadcrumb
     ],
-    handleClick: () => alert('Breadcrumb clicked!'),
+    handleClick: () => console.log('Breadcrumb clicked!'),
+};
+
+export const LongBreadcrumbs = Template.bind({});
+LongBreadcrumbs.args = {
+    datalist: [
+        { title: 'Home', link: '/' },
+        { title: 'Nature', link: '/nature' },
+        { title: 'Forests', link: '/nature/forests' },
+        { title: 'Tropical Rainforests', link: '/nature/forests/tropical' },
+        { title: 'Amazon Rainforest' }, // No link for the active breadcrumb
+    ],
+    handleClick: () => console.log('Breadcrumb clicked!'),
+};
+
+export const NoLinks = Template.bind({});
+NoLinks.args = {
+    datalist: [
+        { title: 'Home' }, // No link
+        { title: 'About Us' }, // No link
+        { title: 'Contact' }, // No link
+    ],
+    handleClick: () => console.log('Breadcrumb clicked!'),
 };
