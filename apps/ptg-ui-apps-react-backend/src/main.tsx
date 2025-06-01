c99aec777ab915f7fd33315664d6db4ac512e84a
Krishna Vaibhav <krishna.vaibhav@yash.com>
2025-06-01 23:41:27 +0530

    Changes related to sonar qube

--- apps/ptg-ui-apps-react-backend/src/main.tsx
+++ apps/ptg-ui-apps-react-backend/src/main.tsx
@@ -5,9 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './app/app';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
     <BrowserRouter>
