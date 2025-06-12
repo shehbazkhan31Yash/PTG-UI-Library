0e1b3afb3751220ba130b954b3654d180d4e7354
MD Faiyazul Islam <faiyazul.islam@yash.com>
2025-06-12 11:13:59 +0530

    fix for shehbaz comment

--- apps/ptg-ui-apps-react-backend/src/app/components/Teams.tsx
+++ apps/ptg-ui-apps-react-backend/src/app/components/Teams.tsx
@@ -1,35 +1,8 @@
 import React from 'react';
-import TeamCard from './TeamCard/TeamCard';
-import ux_Image from '../../assets/images/ux_team.png';
+
 import { Card, Col, Row } from 'react-bootstrap';
 import { contactsTeams } from '@ptg-ui-apps-react-backend/constants/constants';
 const Teams: React.FC = () => {
-  const teamMembersSampleData = [
-    {
-      memberImage: ux_Image,
-      memberName: 'Michelle Stricker',
-      qualification: 'B.S',
-      designation: 'UI Lead',
-    },
-    {
-      memberImage: ux_Image,
-      memberName: 'Valentina Ionova',
-      qualification: 'Ph.D',
-      designation: 'UI Specialist',
-    },
-    {
-      memberImage: ux_Image,
-      memberName: 'Travis Feaker',
-      qualification: 'M.S',
-      designation: 'UI/UX Engineer',
-    },
-    {
-      memberImage: ux_Image,
-      memberName: 'Mohamed Elmansy',
-      qualification: 'Ph.D',
-      designation: 'UI Associate',
-    },
-  ];
   return (
     <section id="teams" className="text-light mt-2 p-4 pt-2">
       <h2 className="text-center text-warning pb-2">Meet Our Group</h2>
@@ -46,18 +19,6 @@ const Teams: React.FC = () => {
               </Card.Body>
             </Card>
           </Col>
-        ))}
-      </Row>
-
-      <Row className="pt-4">
-        {teamMembersSampleData.map((data, index) => (
-          <TeamCard
-            key={data.memberName}
-            memberImage={data.memberImage}
-            memberName={data.memberName}
-            qualification={data.qualification}
-            designation={data.designation}
-          />
         ))}
       </Row>
     </section>
